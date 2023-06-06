088057a5900e50d77c53333abf16d65910f251b4
Alexlin7 <hej2ub548htdlkh@gmail.com>
2023-06-06 20:01:29 +0800

    TDD practice reflection 完成一半

---
+++ reflection/reflection.go
@@ -0,0 +1,40 @@
+package reflection
+
+import "reflect"
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
+
+	}
+
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
